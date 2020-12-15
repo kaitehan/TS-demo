6e8d7237f12b43b350233f8905627eeffef37c42
kaite_han@163.com <kaite_han@163.com>
2020-12-15 20:00:08 +0800

    all done

--- main.ts
+++ main.ts
@@ -2,7 +2,24 @@ var div = document.createElement('div');
 div.id = 'demo'
 document.body.appendChild(div);
 
-div.addEventListener('mousedown',(e)=>{
-    console.log(e.clientX,e.clientY);
-    
+div.addEventListener('mousedown', (e) => {
+    // console.log(e.clientX,e.clientY);
+    var x = e.pageX - div.offsetLeft;
+    var y = e.pageY - div.offsetTop;
+    console.log(111);
+
+    document.addEventListener('mousemove', move);
+
+    function move(e) {
+        div.style.top = e.pageY - y + 'px';
+        div.style.left = e.pageX - x + 'px';
+        document.addEventListener('selectstart', (e) => {
+            e.preventDefault();
+        })
+    }
+
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', move);
+    })
+
 })
