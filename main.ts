5c0ec1c5d0dc63408220f3d318171707510c19e8
kaite_han@163.com <kaite_han@163.com>
2020-12-15 18:57:15 +0800

    鼠标事件

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log(11111);
+var div = document.createElement('div');
+div.id = 'demo'
+document.body.appendChild(div);
+
+div.addEventListener('mousedown',(e)=>{
+    console.log(e.clientX,e.clientY);
+    
+})
